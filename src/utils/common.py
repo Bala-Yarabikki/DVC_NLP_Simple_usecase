73ff9d624734228059521d9ae6734cc0b2696b0c
Bala <balachowdary777@gmail.com>
2021-11-03 06:36:09 +0100

    stage_02

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,8 +1,9 @@
-import yaml
 import os
-import json
+import yaml
 import logging
 import time
+import pandas as pd
+import json
 
 
 def read_yaml(path_to_yaml: str) -> dict:
@@ -12,7 +13,26 @@ def read_yaml(path_to_yaml: str) -> dict:
     return content
 
 
-def create_directories(dirs: list):
-    for dir_path in dirs:
-        os.makedirs(dir_path, exist_ok=True)
-        logging.info(f"directory is created at {dir_path}")
+def create_directories(path_to_directories: list) -> None:
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        logging.info(f"created directory at: {path}")
+
+
+def get_df(path_to_data: str, sep: str = "\t") -> pd.DataFrame:
+    df = pd.read_csv(
+        path_to_data,
+        encoding="utf-8",
+        header=None,
+        delimiter=sep,
+        names=["id", "label", "text"],
+    )
+    logging.info(f"The input data frame {path_to_data} size is {df.shape}\n")
+    return df
+
+
+def save_json(path, data):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+    logging.info(f"json file saved at: {path}")
